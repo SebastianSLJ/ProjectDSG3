12cc995f42e37cd4ecbfcb23998b1a8ce6da07c3
Zanat07 <152235211+Zanat07@users.noreply.github.com>
2023-11-30 12:34:00 -0500

    Update carousel.js

--- templates/js/carousel.js
+++ templates/js/carousel.js
@@ -4,16 +4,16 @@ let images = [];
 let time = 1000;
 
 images[0] = "./static/images/1.jpg";
-images[1] = "./static/images/2.jpg";
-images[2] = "./static/images/3.jpg";
-images[3] = "./static/images/4.jpg";
-images[4] = "./static/images/5.jpg";
-images[5] = "./static/images/6.jpg";
-images[6] = "./static/images/7.jpg";
-images[7] = "./static/images/8.jpg";
-images[8] = "./static/images/9.jpg";
-images[9] = "./static/images/10.jpg";
-images[10] = "./static/images/11.jpg";
+images[1] = "./static/images/2.png";
+images[2] = "./static/images/3.png";
+images[3] = "./static/images/4.png";
+images[4] = "./static/images/5.png";
+images[5] = "./static/images/6.png";
+images[6] = "./static/images/7.png";
+images[7] = "./static/images/8.png";
+images[8] = "./static/images/9.png";
+images[9] = "./static/images/10.png";
+images[10] = "./static/images/11.png";
 
 function change() {
     document.carousel.src = images[i];
